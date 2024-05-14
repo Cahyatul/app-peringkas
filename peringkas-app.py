91b22d6d5c2b6112abd92ce62aaacdd8afb6de06
Cahyatul <119913848+Cahyatul@users.noreply.github.com>
2024-05-15 06:02:51 +0700

    Update peringkas-app.py

--- peringkas-app.py
+++ peringkas-app.py
@@ -6,23 +6,12 @@ import requests
 st.title('Welcome to Text Summary')
 st.header('ringkas artikel dengan mudah dan cepat')
 
-def get_text_from_url(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            paragraphs = soup.find_all('p')
-            article_text = ' '.join(p.get_text() for p in paragraphs)
-            return article_text
-        else:
-            return f"Error: Status code {response.status_code}"
-    except requests.RequestException as e:
-        return f"Request failed: {e}"
+
 
 # Input URL
 url_input = st.text_input("Masukkan URL artikel")
 
-
+# tombol peringkas
 text = ''
 if st.button('Lihat Teks'):
     if url_input:
