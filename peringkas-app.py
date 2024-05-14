eaef2cec7518840f8cbc2afa48ad0adb1ab5865b
Cahyatul <119913848+Cahyatul@users.noreply.github.com>
2024-05-14 20:42:13 +0700

    Update peringkas-app.py

--- peringkas-app.py
+++ peringkas-app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+from bs4 import BeautifulSoup
 
 
 st.title('Welcome to Text Summary')
 st.header('ringkas artikel dengan mudah dan cepat')
+
+elif option == "Input URL":
+    url = st.text_input("Masukkan URL:")
+    if st.button("Ringkas URL"):
+        if url:
+            summary = summarize_url(url)
+            st.subheader("Ringkasan URL:")
+            st.write(summary)
+        else:
+            st.error("URL tidak valid.")
