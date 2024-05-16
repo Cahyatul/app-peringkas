3c31063c75fdc770073e154961fda34a7f9bd015
chytl <119913848+Cahyatul@users.noreply.github.com>
2024-05-16 07:09:43 +0700

    Update peringkas-app.py

--- peringkas-app.py
+++ peringkas-app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from bs4 import BeautifulSoup
 import requests
-from transformers import pipeline
+from PyPDF2 import PdfReader
+from docx import Document
+from bs4 import BeautifulSoup
 import re
 import nltk
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
@@ -12,9 +13,10 @@ from sumy.summarizers.text_rank import TextRankSummarizer
 # Mengunduh tokenizer untuk bahasa Inggris (yang dapat digunakan untuk teks Indonesia)
 nltk.download('punkt')
 
-st.title('Welcome to Text Summary')
-st.header('ringkas artikel dengan mudah dan cepat')
+st.subheader('Selamat Datang Di Aplikasi RingkasAja.ID',divider='rainbow')
+st.write("Solusi Meringkas Cepat dijamin Akurat")
 
+# Fungsi untuk mengambil teks dari URL
 def get_text_from_url(url):
     try:
         response = requests.get(url)
@@ -27,8 +29,6 @@ def get_text_from_url(url):
             return f"Error: Status code {response.status_code}"
     except requests.RequestException as e:
         return f"Request failed: {e}"
-        
-
 
 # Fungsi untuk pembersihan teks
 def clean_text(text):
@@ -58,20 +58,32 @@ def summarize_text(text):
     summary = summarizer(parser.document, 3)  # Merangkum menjadi 3 kalimat
     return ' '.join([str(sentence) for sentence in summary])
 
-
 # Input URL
-url_input = st.text_input("Masukkan URL")
+url_input = st.text_input("Masukkan URL artikel")
 
-# tombol peringkas
-text = ''
+# Input File
+uploaded_file = st.file_uploader("Unggah Dokumen (PDF atau DOCX)")
+
+# Tombol untuk menampilkan teks
 if st.button('Lihat Teks'):
     text = ''
     if url_input:
         # Proses URL
         text = get_text_from_url(url_input)
-     
-        
-  if text:
+    elif uploaded_file:
+        # Proses File
+        if uploaded_file.name.endswith('.pdf'):
+            reader = PdfReader(uploaded_file)
+            for page in reader.pages:
+                text += page.extract_text()
+        elif uploaded_file.name.endswith('.docx'):
+            doc = Document(uploaded_file)
+            for para in doc.paragraphs:
+                text += para.text
+        else:
+            st.error('Format file tidak didukung.')
+    
+    if text:
         text = clean_text(text)
         text = remove_stopwords(text)
         sentences = split_sentences(text)
@@ -81,8 +93,7 @@ if st.button('Lihat Teks'):
     else:
         st.error('Silakan masukkan URL atau unggah file.')
 
-
-        # Tombol untuk menampilkan ringkasan
+# Tombol untuk menampilkan ringkasan
 if st.button('Tampilkan Ringkasan'):
     if 'text' in st.session_state:
         summary = summarize_text(st.session_state.text)
