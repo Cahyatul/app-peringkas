9915e18b564907ed61436389a22a2ba9a43d494a
chytl <119913848+Cahyatul@users.noreply.github.com>
2024-05-18 12:14:54 +0700

    Update peringkas-app.py

--- peringkas-app.py
+++ peringkas-app.py
@@ -66,5 +66,16 @@ if st.button('Lihat Teks'):
     if url_input:
         # Proses URL
         text = get_text_from_url(url_input)
+
+ 
+    if text:
+        text = clean_text(text)
+        text = remove_stopwords(text)
+        sentences = split_sentences(text)
+        tokens = tokenize_text(sentences)
+        st.session_state.text = ' '.join([' '.join(token) for token in tokens])
+        st.write(st.session_state.text)
+    else:
+        st.error('Silakan masukkan URL atau unggah file.')
        
   
