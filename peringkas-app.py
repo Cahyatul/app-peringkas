0bc1a6e4a4745710e4894023e70e8d24628b9b21
chytl <119913848+Cahyatul@users.noreply.github.com>
2024-05-18 09:53:23 +0700

    Update peringkas-app.py

--- peringkas-app.py
+++ peringkas-app.py
@@ -24,30 +24,56 @@ def get_text_from_url(url):
     except requests.RequestException as e:
         return f"Request failed: {e}"
 
+# Fungsi untuk pembersihan teks
+def clean_text(text):
+    text = re.sub(r'\[.*?\]', '', text)  # Menghapus teks dalam tanda kurung
+    text = re.sub(r'\s+', ' ', text)  # Menghapus spasi berlebih
+    text = text.lower()  # Mengubah teks menjadi huruf kecil
+    return text
+
+# Fungsi untuk menghapus stopwords
+def remove_stopwords(text):
+    factory = StopWordRemoverFactory()
+    stopword_remover = factory.create_stop_word_remover()
+    return stopword_remover.remove(text)
+
+# Fungsi untuk memisahkan teks menjadi kalimat-kalimat
+def split_sentences(text):
+    return nltk.sent_tokenize(text)
+
+# Fungsi untuk tokenisasi teks
+def tokenize_text(sentences):
+    return [nltk.word_tokenize(sentence) for sentence in sentences]
+
+# Fungsi untuk meringkas teks menggunakan TextRank
+def summarize_text(text):
+    parser = PlaintextParser.from_string(text, SumyTokenizer("english"))
+    summarizer = TextRankSummarizer()
+    summary = summarizer(parser.document, 3)  # Merangkum menjadi 3 kalimat
+    return ' '.join([str(sentence) for sentence in summary])
+
 user_input = st.text_input("masukkan teks")
 
 # Input URL
 url_input = st.text_input("Masukkan URL")
 
-# Fungsi untuk membersihkan teks
-def clean_text(text):
-    # Menghapus data dalam tanda kurung siku
-    text = re.sub(r'\[.*?\]', '', text)
-    # Menghapus spasi ekstra
-    text = re.sub(r'\s+', ' ', text)
-    return text
 
-# tombol peringkas
-text = ''
-if st.button('Ringkas Teks'):
-    if url_input:
-        # Proses URL
-        text = get_text_from_url(url_input, user_input )
-        text = clean_text(text)  # Membersihkan teks
-        st.write(text)
 
-# Fungsi peringkas teks
-def summarize_text(text):
-    summarizer = pipeline("summarization")
-    summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
-    return summary[0]['summary_text']
+
+ if text:
+        text = clean_text(text)
+        text = remove_stopwords(text)
+        sentences = split_sentences(text)
+        tokens = tokenize_text(sentences)
+        st.session_state.text = ' '.join([' '.join(token) for token in tokens])
+        st.write(st.session_state.text)
+    else:
+        st.error('Silakan masukkan URL')
+
+# Tombol untuk menampilkan ringkasan
+if st.button('Tampilkan Ringkasan'):
+    if 'text' in st.session_state:
+        summary = summarize_text(st.session_state.text)
+        st.write(summary)
+    else:
+        st.error('Masukkan teks untuk diringkas.')
